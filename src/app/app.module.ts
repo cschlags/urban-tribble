b0bbddd782ae531405ea5e7ebbf0e3d11358437e
Christina Leuci <christinaleuci@gmail.com>
2019-09-12 21:57:03 -0400

    Implement Map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoiY2hyaXN0aW5hLWxldWNpIiwiYSI6ImNrMGhncnZ1bDAwOXEzaG1nMHh0eDNqbmUifQ.zEAy5yFM3C5XAhuzr-7I5A', // Optionnal, can also be set per map (accessToken input of mgl-map)
+      geocoderAccessToken: 'TOKEN' // Optionnal, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
