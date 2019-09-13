6e4507ae8e5054eda0a5f786954c740ba94d252b
Christina Leuci <christinaleuci@gmail.com>
2019-09-12 22:22:10 -0400

    Implement Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,9 +18,11 @@ import { MapComponent } from './map/map.component';
     BrowserModule,
     AppRoutingModule,
     NgxMapboxGLModule.withConfig({
-      accessToken: 'pk.eyJ1IjoiY2hyaXN0aW5hLWxldWNpIiwiYSI6ImNrMGhncnZ1bDAwOXEzaG1nMHh0eDNqbmUifQ.zEAy5yFM3C5XAhuzr-7I5A', // Optionnal, can also be set per map (accessToken input of mgl-map)
+      accessToken: environment.mapbox.apiKey, // Optionnal, can also be set per map (accessToken input of mgl-map)
       geocoderAccessToken: 'TOKEN' // Optionnal, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
-    })
+    }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
